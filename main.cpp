98ff807866ba1f405cff6eb7a505cc286d796481
Isaac Lourenço <isaac.lourenco.704@ufrn.edu.br>
2023-10-14 20:21:25 -0300

    add point, rect and bin structs

--- main.cpp
+++ main.cpp
@@ -6,55 +6,58 @@ using namespace std;
 #define rect pair<float, float>
 #define vect vector<rect> 
 
+struct Point {
+    float x;
+    float y;
+};
+
+struct Rect {
+    float width;
+    float height;
+};
+
+struct Bin {
+    float width;
+    float height;
+    vector<pair<Point, Point>> cuts;
+};
+
 int main() {
     int qnt_rectangles;
     cin >> qnt_rectangles;
 
-    vect rectangles;
-    vect bins;
+    vector<Rect> rectangles;
+    vector<Bin> bins;
 
     for(int i = 0; i < qnt_rectangles; ++i){
         float width, height;
         cin >> width >> height;
-        rect rectangle = { width, height };
+        Rect rectangle;
+        rectangle.width = width;
+        rectangle.height = height;
         rectangles.push_back(rectangle);
     }
 
-    vect rectangles_by_width(rectangles);
+    vector<Rect> rectangles_by_width(rectangles);
     // Order by first element (width) non-cresc
     sort(rectangles_by_width.begin(), rectangles_by_width.end(), [](const auto& left, const auto& right) {
-        return left.first > right.first;
+        return left.width > right.width;
     });
 
-    vect rectangles_by_height(rectangles);
+    vector<Rect> rectangles_by_height(rectangles);
     // Order by second element (height) non-cresc
     sort(rectangles_by_height.begin(), rectangles_by_height.end(), [](const auto& left, const auto& right) {
-        return left.second > right.second;
+        return left.height > right.height;
     });
 
     // And then was bin...
     
-    //        ____
-    //  ______|H |
-    //  |  W  |  |
-    //  ⁻⁻⁻⁻⁻⁻⁻⁻⁻⁻
-    if (rectangles_by_width[0].second + rectangles_by_height[0].second <= MAX_RECT_DIMENSION_SIZE) {
-        bins.emplace_back(rectangles_by_width[0].second + rectangles_by_height[0].second, rectangles_by_height[0].first);
-    }
-    //  ___
-    //  |H |    
-    //  |__|____
-    //  |  W   |
-    //  ⁻⁻⁻⁻⁻⁻⁻⁻
-    else if (rectangles_by_width[0].first + rectangles_by_height[0].first <= MAX_RECT_DIMENSION_SIZE) {
-        bins.emplace_back(rectangles_by_width[0].second + rectangles_by_height[0].second, rectangles_by_height[0].first);
-    }
-    // Need two bins...
-    else {
-        rect bin = {rectangles_by_width[0].first, rectangles_by_height[0].first};
-        bins.emplace_back(bin);
-        bins.emplace_back(bin);
-    }    
+    float bin_dimension = max(rectangles_by_width[0].width, rectangles_by_height[0].height);
+    Bin bin;
+    bin.width = bin_dimension;
+    bin.height = bin_dimension;
+    bins.push_back(bin);
 
-    cout << bins.size() << '\n';
+    cout << bin_dimension << '\n';
+
 }
