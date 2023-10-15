0945566e5d41946e240c3b6a5acdb9150d2b622f
Isaac Lourenço <isaac.lourenco.704@ufrn.edu.br>
2023-10-14 21:44:06 -0300

    stub width-height-first-fit

--- main.cpp
+++ main.cpp
@@ -11,9 +11,10 @@ struct Point {
     float y;
 };
 
-struct Rect {
+struct Rectangle {
     float width;
     float height;
+    int id;
 };
 
 struct Bin {
@@ -22,29 +23,45 @@ struct Bin {
     vector<pair<Point, Point>> cuts;
 };
 
+Bin createSquareBin(float dim) {
+    Bin bin;
+    bin.width = dim;
+    bin.height = dim;
+    return bin;
+}
+
+int find_bin_fits(Rectangle rectangle){
+    return -1; // STUB
+}
+
+void insertRectangleOnBinIndex(Rectangle rectangle, int i_bin){
+    // STUB
+}
+
 int main() {
     int qnt_rectangles;
     cin >> qnt_rectangles;
 
-    vector<Rect> rectangles;
+    vector<Rectangle> rectangles;
     vector<Bin> bins;
 
     for(int i = 0; i < qnt_rectangles; ++i){
         float width, height;
         cin >> width >> height;
-        Rect rectangle;
+        Rectangle rectangle;
         rectangle.width = width;
         rectangle.height = height;
+        rectangle.id = i;
         rectangles.push_back(rectangle);
     }
 
-    vector<Rect> rectangles_by_width(rectangles);
+    vector<Rectangle> rectangles_by_width(rectangles);
     // Order by first element (width) non-cresc
     sort(rectangles_by_width.begin(), rectangles_by_width.end(), [](const auto& left, const auto& right) {
         return left.width > right.width;
     });
 
-    vector<Rect> rectangles_by_height(rectangles);
+    vector<Rectangle> rectangles_by_height(rectangles);
     // Order by second element (height) non-cresc
     sort(rectangles_by_height.begin(), rectangles_by_height.end(), [](const auto& left, const auto& right) {
         return left.height > right.height;
@@ -53,11 +70,37 @@ int main() {
     // And then was bin...
     
     float bin_dimension = max(rectangles_by_width[0].width, rectangles_by_height[0].height);
-    Bin bin;
-    bin.width = bin_dimension;
-    bin.height = bin_dimension;
+    Bin bin = createSquareBin(bin_dimension);
     bins.push_back(bin);
 
-    cout << bin_dimension << '\n';
+    map<int, bool> id_rectangle_was_inserted;
+    int i_widths = 0, i_heights = 0;
+    bool put_widther = true;
+    while (i_widths < qnt_rectangles || i_heights < qnt_rectangles){
+        Rectangle rectangle;
+        if (put_widther && i_widths < qnt_rectangles) {
+            rectangle = rectangles_by_width[i_widths];
+            ++i_widths;
+        } else if (i_heights < qnt_rectangles) {
+            rectangle = rectangles_by_height[i_heights];
+            ++i_heights;
+        }
 
+        if(id_rectangle_was_inserted[rectangle.id])
+            continue;
+
+        id_rectangle_was_inserted[rectangle.id] = true;
+
+        cout << rectangle.width << ' ' << rectangle.height << endl;
+
+        int i_bin = find_bin_fits(rectangle);
+        if (i_bin == -1) {
+            auto bin = createSquareBin(bin_dimension);
+            bins.push_back(bin);
+        }
+        insertRectangleOnBinIndex(rectangle, bins.size() - 1);
+        put_widther != put_widther; // alternate by width/height rects
+    }
+    
+    cout << bins.size() << endl;
 }
