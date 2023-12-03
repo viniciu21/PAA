a2d43080003824a1f51f58522dd2c10755e4bbee
Isaac Lourenço <isaac.lourenco.704@ufrn.edu.br>
2023-12-03 14:53:59 -0300

    local_search now uses next fit

--- local_search.cpp
+++ local_search.cpp
@@ -1,28 +1,5 @@
 #include "local_search.h"
 
-
-vector<Bin> sequencing_generate_bins(const vector<double> & items) {
-    int qnt_bins = 1;
-    vector<Bin> bins;
-    bins.push_back({0, 1, 0, 1, 0, qnt_bins});
-
-    double current_sum = 0;
-    for (int i = 0; i < items.size(); ++i) {
-        auto item = items[i];
-
-        if (current_sum + item > 1) {
-            current_sum = 0;
-            ++qnt_bins;
-            bins.push_back({0, 1, 0, 1, 0, qnt_bins});
-        }
-
-        current_sum += item;
-        bins.back().rect_inside.push_back({1, (float) item, 0, 0, 0, i});
-    }
-
-    return bins;
-}
-
 vector<double> get_sequencing_items_from_bins(const vector<Bin> & bins) {
     vector<double> items;
     for (auto bin : bins) {
@@ -39,23 +16,8 @@ vector<Bin> swap_and_generate_bins(int a, int b, vector<Bin> bins) {
     return sequencing_generate_bins(items);
 }
 
-vect rectangles_from_items(const vector<double> & items) {
-    vect rectangles;
-    
-    for (int i = 0; i < items.size(); ++i) {
-        Rectangle rectangle;
-        rectangle.width = 1;
-        rectangle.height = items[i];
-        rectangle.id = i + 1;
-        rectangles.push_back(rectangle);
-    }
-
-    return rectangles;
-}
-
 vector<Bin> local_search(const vector<double> & items) {
-    vect rectangles = rectangles_from_items(items); 
-    vector<Bin> solution = first_fit_decreasing_height(rectangles);
+    vector<Bin> solution = next_fit_decreasing_height(items);
     vector<Bin> best_solution = solution;
 
     bool can_improve = true;
@@ -76,20 +38,3 @@ vector<Bin> local_search(const vector<double> & items) {
 
     return solution;
 }
-
-// int main() {
-//     srand(time(0)); 
-
-//     int qnt_items;
-//     cin >> qnt_items;
-    
-//     vector<double> items;
-//     for(int i = 0; i < qnt_items; ++i) {
-//         double item;
-//         cin >> item;
-//         items.push_back(item);
-//     }
-
-//     cout << local_search(items).size() << '\n';
-//     return 0;
-// }
--- next_fit_decreasing_height.cpp
+++ next_fit_decreasing_height.cpp
@@ -1,5 +1,27 @@
 #include "next_fit_decreasing_height.h"
 
+vector<Bin> sequencing_generate_bins(const vector<double> & items) {
+    int qnt_bins = 1;
+    vector<Bin> bins;
+    bins.push_back({0, 1, 0, 1, 0, qnt_bins});
+
+    double current_sum = 0;
+    for (int i = 0; i < items.size(); ++i) {
+        auto item = items[i];
+
+        if (current_sum + item > 1) {
+            current_sum = 0;
+            ++qnt_bins;
+            bins.push_back({0, 1, 0, 1, 0, qnt_bins});
+        }
+
+        current_sum += item;
+        bins.back().rect_inside.push_back({1, (float) item, 0, 0, 0, i});
+    }
+
+    return bins;
+}
+
 vector<Bin> next_fit_decreasing_height(vector<double> items) {
     sort(items.begin(), items.end(), greater<double>());
     return sequencing_generate_bins(items);
